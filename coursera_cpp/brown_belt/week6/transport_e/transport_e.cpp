860b9f3fc3d43fd73b7108d5315c235f9bc1d0eb
Sergey <sbachev@aorti.ru>
2020-09-23 14:56:43 +0300

    не доделал финальную задачу коричневого пояса, часть e

--- coursera_cpp/brown_belt/week6/transport_e/transport_e.cpp
+++ coursera_cpp/brown_belt/week6/transport_e/transport_e.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <exception>
 #include <iostream>
+#include <memory>
 #include <optional>
 #include <set>
 #include <string>
@@ -9,7 +10,9 @@
 #include <utility>
 #include <vector>
 
+#include "graph.h"
 #include "json.h"
+#include "router.h"
 #include "test_runner.h"
 
 using namespace std;
@@ -28,6 +31,7 @@ enum class JsonKeys {
   ID,
   BASE_REQS,
   STAT_REQS,
+  ROUTE_SETTS,
   TYPE,
   ROAD_DIST,
   NAME,
@@ -36,6 +40,8 @@ enum class JsonKeys {
   LON,
   STOPS,
   REQ_ID,
+  WAIT_T,
+  VELOCITY,
 };
 
 const unordered_map<JsonKeys, string> JSON_KEYS = {
@@ -48,6 +54,7 @@ const unordered_map<JsonKeys, string> JSON_KEYS = {
     {JsonKeys::ID, "id"},
     {JsonKeys::BASE_REQS, "base_requests"},
     {JsonKeys::STAT_REQS, "stat_requests"},
+    {JsonKeys::ROUTE_SETTS, "routing_settings"},
     {JsonKeys::TYPE, "type"},
     {JsonKeys::ROAD_DIST, "road_distances"},
     {JsonKeys::NAME, "name"},
@@ -56,6 +63,8 @@ const unordered_map<JsonKeys, string> JSON_KEYS = {
     {JsonKeys::LON, "longitude"},
     {JsonKeys::STOPS, "stops"},
     {JsonKeys::REQ_ID, "request_id"},
+    {JsonKeys::WAIT_T, "bus_wait_time"},
+    {JsonKeys::VELOCITY, "bus_velocity"},
 };
 
 struct Stop {
@@ -64,6 +73,8 @@ struct Stop {
   double lon_ = 0.0;
   set<string> buses_ = {};
   unordered_map<string, size_t> distanceToStops_ = {};
+  Graph::VertexId from_ = 0;
+  Graph::VertexId to_ = 0;
 };
 
 double degToRad(double deg) {
@@ -82,9 +93,9 @@ double computeDistance(const Stop& lhs, const Stop& rhs) {
          EARTH_RADIUS;
 }
 
-class BusRoute {
+class Bus {
  public:
-  BusRoute(const string& routeNumber, bool isRing, vector<string> stopNames)
+  Bus(const string& routeNumber, bool isRing, vector<string> stopNames)
       : number_(routeNumber),
         isRing_(isRing),
         stopNames_(move(stopNames)),
@@ -123,37 +134,99 @@ class BusRoute {
 };
 
 using Stops = unordered_map<string, Stop>;
-using Routes = unordered_map<string, BusRoute>;
+using Buses = unordered_map<string, Bus>;
+using FloatGraph = Graph::DirectedWeightedGraph<float>;
+using FloatRouter = Graph::Router<float>;
 
-class BusManager {
+class TransportCatalog {
  public:
-  BusManager(Stops stops, Routes routes)
-      : stops_(move(stops)), routes_(move(routes)) {}
+  TransportCatalog(Stops stops, Buses buses, int waitTime, int velocity)
+      : stops_(move(stops)),
+        buses_(move(buses)),
+        waitTime_(waitTime),
+        velocity_(velocity),
+        graphPtr_(),
+        routerPtr_(),
+        waitActs_(),
+        moveActs_() {
+    graphPtr_ = make_unique<FloatGraph>(stops_.size() * 2);
 
-  unordered_map<string, Json::Node> routeInfo(const string& routeNumber,
-                                              int id) {
+    for (const auto& [busNames, bus] : buses_) {
+      const auto& stopsNames = bus.stopsNames();
+      for (size_t i = 0; i < stopsNames.size() - 1; ++i) {
+        const auto& first = stops_.at(stopsNames[i]);
+        const auto& second = stops_.at(stopsNames[i + 1]);
+
+        Graph::Edge<float> firstWaitEdge{first.from_, first.to_, waitTime_};
+        const auto firstWaitEdgeId = graphPtr_->AddEdge(firstWaitEdge);
+        waitActs_[firstWaitEdgeId] = firstWaitEdge;
+
+        float time;
+        if (first.distanceToStops_.count(second.name_)) {
+          time = first.distanceToStops_.at(second.name_) / velocity_;
+        } else {
+          time = second.distanceToStops_.at(first.name_) / velocity_;
+        }
+        Graph::Edge<float> moveEdge{first.to_, second.from_, time};
+        const auto moveEdgeId = graphPtr_->AddEdge(moveEdge);
+        moveActs_[moveEdgeId] = moveEdge;
+
+        Graph::Edge<float> secondWaitEdge{second.from_, second.to_, waitTime_};
+        const auto secondWaitEdgeId = graphPtr_->AddEdge(secondWaitEdge);
+        waitActs_[secondWaitEdgeId] = secondWaitEdge;
+      }
+
+      if (!bus.isRing()) {
+        for (size_t i = stopsNames.size() - 1; i > 0; --i) {
+          const auto& first = stops_.at(stopsNames[i]);
+          const auto& second = stops_.at(stopsNames[i - 1]);
+
+          Graph::Edge<float> firstWaitEdge{first.from_, first.to_, waitTime_};
+          const auto firstWaitEdgeId = graphPtr_->AddEdge(firstWaitEdge);
+          waitActs_[firstWaitEdgeId] = firstWaitEdge;
+
+          float time;
+          if (first.distanceToStops_.count(second.name_)) {
+            time = first.distanceToStops_.at(second.name_) / velocity_;
+          } else {
+            time = second.distanceToStops_.at(first.name_) / velocity_;
+          }
+          Graph::Edge<float> moveEdge{first.to_, second.from_, time};
+          const auto moveEdgeId = graphPtr_->AddEdge(moveEdge);
+          moveActs_[moveEdgeId] = moveEdge;
+
+          Graph::Edge<float> secondWaitEdge{second.from_, second.to_,
+                                            waitTime_};
+          const auto secondWaitEdgeId = graphPtr_->AddEdge(secondWaitEdge);
+          waitActs_[secondWaitEdgeId] = secondWaitEdge;
+        }
+      }
+    }
+  }
+
+  unordered_map<string, Json::Node> busInfo(const string& routeNumber, int id) {
     unordered_map<string, Json::Node> result;
     result[JSON_KEYS.at(JsonKeys::TYPE)] = Json::Node("Bus"s);
     result[JSON_KEYS.at(JsonKeys::REQ_ID)] = Json::Node(id);
 
-    if (routes_.count(routeNumber)) {
-      auto stops = routes_.at(routeNumber).stopsCount();
-      auto uniqueStops = routes_.at(routeNumber).uniqueStopsCount();
+    if (buses_.count(routeNumber)) {
+      auto stops = buses_.at(routeNumber).stopsCount();
+      auto uniqueStops = buses_.at(routeNumber).uniqueStopsCount();
 
-      auto lengthByData = routes_.at(routeNumber).lengthByData();
+      auto lengthByData = buses_.at(routeNumber).lengthByData();
       if (!lengthByData) {
         auto routeLength = computeLengthByData(routeNumber);
-        routes_.at(routeNumber).setLengthByData(routeLength);
+        buses_.at(routeNumber).setLengthByData(routeLength);
 
         auto lengthByCoordinates = computeLengthByCoordinates(routeNumber);
         auto curvature = routeLength / lengthByCoordinates;
-        routes_.at(routeNumber).setCurvature(curvature);
+        buses_.at(routeNumber).setCurvature(curvature);
       }
 
       result[JSON_KEYS.at(JsonKeys::ROUTE_LNGTH)] =
-          Json::Node(static_cast<int>(*routes_.at(routeNumber).lengthByData()));
+          Json::Node(static_cast<int>(*buses_.at(routeNumber).lengthByData()));
       result[JSON_KEYS.at(JsonKeys::CURV)] =
-          Json::Node(*routes_.at(routeNumber).curvature());
+          Json::Node(*buses_.at(routeNumber).curvature());
       result[JSON_KEYS.at(JsonKeys::STOP_CNT)] =
           Json::Node(static_cast<int>(stops));
       result[JSON_KEYS.at(JsonKeys::UNIQ_STOP_CNT)] =
@@ -185,12 +258,42 @@ class BusManager {
     return result;
   }
 
+  //  unordered_map<string, Json::Node> routeInfo(const pair<string, string>& p,
+  //                                              int id) {
+  //    unordered_map<string, Json::Node> result;
+  //    result[JSON_KEYS.at(JsonKeys::TYPE)] = Json::Node("Route"s);
+  //    result[JSON_KEYS.at(JsonKeys::REQ_ID)] = Json::Node(id);
+
+  //    if (routes_.count(p)) {
+  //      // TODO: return existing route
+  //    } else {
+  //      // TODO: try to calculate route, add to routes_, and return route.
+  //      // If it's impossible to calculate route, return error message
+  //      result[JSON_KEYS.at(JsonKeys::ERR_MSG)] = Json::Node("not found"s);
+  //    }
+
+  //    return result;
+  //  }
+
  private:
+  using RouteActions = unordered_map<Graph::EdgeId, Graph::Edge<float>>;
+
+  struct WaitAction {
+    string stopName_;
+    float time_;
+  };
+
   Stops stops_;
-  Routes routes_;
+  Buses buses_;
+  const float waitTime_;
+  const float velocity_;
+  unique_ptr<FloatGraph> graphPtr_;
+  unique_ptr<FloatRouter> routerPtr_;
+  RouteActions waitActs_;
+  RouteActions moveActs_;
 
   double computeLengthByCoordinates(const string& routeNumber) const {
-    auto stopsNames = routes_.at(routeNumber).stopsNames();
+    auto stopsNames = buses_.at(routeNumber).stopsNames();
     double length = 0.0;
     for (size_t i = 0; i < stopsNames.size() - 1; ++i) {
       const auto& first = stops_.at(stopsNames[i]);
@@ -198,7 +301,7 @@ class BusManager {
       length += computeDistance(first, second);
     }
 
-    if (!routes_.at(routeNumber).isRing()) {
+    if (!buses_.at(routeNumber).isRing()) {
       length *= 2;
     }
 
@@ -206,7 +309,7 @@ class BusManager {
   }
 
   size_t computeLengthByData(const string& routeNumber) const {
-    const auto stopsNames = routes_.at(routeNumber).stopsNames();
+    const auto stopsNames = buses_.at(routeNumber).stopsNames();
     size_t length = 0;
     for (size_t i = 0; i < stopsNames.size() - 1; ++i) {
       const auto& first = stops_.at(stopsNames[i]);
@@ -219,7 +322,7 @@ class BusManager {
       }
     }
 
-    if (!routes_.at(routeNumber).isRing()) {
+    if (!buses_.at(routeNumber).isRing()) {
       for (size_t i = stopsNames.size() - 1; i > 0; --i) {
         const auto& first = stops_.at(stopsNames[i]);
         const auto& second = stops_.at(stopsNames[i - 1]);
@@ -236,11 +339,13 @@ class BusManager {
 };
 }  // namespace
 
-Stop buildStop(const map<string, Json::Node>& reqMap) {
+Stop buildStop(const map<string, Json::Node>& reqMap, Graph::VertexId& ctr) {
   Stop stop;
   stop.name_ = reqMap.at(JSON_KEYS.at(JsonKeys::NAME)).AsString();
   stop.lat_ = reqMap.at(JSON_KEYS.at(JsonKeys::LAT)).AsDouble();
   stop.lon_ = reqMap.at(JSON_KEYS.at(JsonKeys::LON)).AsDouble();
+  stop.from_ = ++ctr;
+  stop.to_ = ++ctr;
   const auto& distancesToStops =
       reqMap.at(JSON_KEYS.at(JsonKeys::ROAD_DIST)).AsMap();
   for (const auto& elem : distancesToStops) {
@@ -250,7 +355,7 @@ Stop buildStop(const map<string, Json::Node>& reqMap) {
   return stop;
 }
 
-BusRoute buildRoute(const map<string, Json::Node>& reqMap) {
+Bus buildBus(const map<string, Json::Node>& reqMap) {
   auto routeNumber = reqMap.at(JSON_KEYS.at(JsonKeys::NAME)).AsString();
   auto isRing = reqMap.at(JSON_KEYS.at(JsonKeys::IS_ROUND)).AsBool();
   const auto& stops = reqMap.at(JSON_KEYS.at(JsonKeys::STOPS)).AsArray();
@@ -259,18 +364,20 @@ BusRoute buildRoute(const map<string, Json::Node>& reqMap) {
     stopNames.push_back(stop.AsString());
   }
 
-  return BusRoute(routeNumber, isRing, move(stopNames));
+  return Bus(routeNumber, isRing, move(stopNames));
 }
 
-BusManager buildDataBase(const vector<Json::Node>& requests) {
+TransportCatalog buildCatalog(const vector<Json::Node>& requests,
+                              const map<string, Json::Node>& routingParams) {
   Stops stops;
-  Routes routes;
+  Buses buses;
 
+  Graph::VertexId ctr = 0;
   for (const auto& req : requests) {
     const auto& reqMap = req.AsMap();
     const auto& type = reqMap.at(JSON_KEYS.at(JsonKeys::TYPE)).AsString();
     if (type == "Stop") {
-      auto stop = buildStop(reqMap);
+      auto stop = buildStop(reqMap, ctr);
       if (stops.count(stop.name_)) {
         stops[stop.name_].lat_ = stop.lat_;
         stops[stop.name_].lon_ = stop.lon_;
@@ -280,18 +387,20 @@ BusManager buildDataBase(const vector<Json::Node>& requests) {
         stops[stop.name_] = move(stop);
       }
     } else if (type == "Bus") {
-      auto busRoute = buildRoute(reqMap);
-      auto routeNumber = busRoute.routeNumber();
-      for (const auto& stopName : busRoute.stopsNames()) {
+      auto bus = buildBus(reqMap);
+      auto routeNumber = bus.routeNumber();
+      for (const auto& stopName : bus.stopsNames()) {
         stops[stopName].buses_.insert(routeNumber);
       }
       // TODO: почему не работает operator[]
       // routes[move(routeNumber)] = move(busRoute);
-      routes.insert({routeNumber, move(busRoute)});
+      buses.insert({routeNumber, move(bus)});
     }
   }
 
-  return {stops, routes};
+  auto waitTime = routingParams.at(JSON_KEYS.at(JsonKeys::WAIT_T)).AsInt();
+  auto velocity = routingParams.at(JSON_KEYS.at(JsonKeys::VELOCITY)).AsInt();
+  return {stops, buses, waitTime, velocity};
 }
 
 string printReq(const unordered_map<string, Json::Node>& req) {
@@ -336,6 +445,8 @@ string printReq(const unordered_map<string, Json::Node>& req) {
       }
 
       outStream << "]";
+    } else if (type == "Route") {
+      // TODO: fill outStream
     }
   }
 
@@ -343,7 +454,7 @@ string printReq(const unordered_map<string, Json::Node>& req) {
   return outStream.str();
 }
 
-void processRequests(BusManager& manager,
+void processRequests(TransportCatalog& manager,
                      const vector<Json::Node>& requests,
                      ostream& outStream = cout) {
   outStream.precision(6);
@@ -361,58 +472,21 @@ void processRequests(BusManager& manager,
     const auto& type = reqMap.at(JSON_KEYS.at(JsonKeys::TYPE)).AsString();
     const auto id = reqMap.at(JSON_KEYS.at(JsonKeys::ID)).AsInt();
     if (type == "Bus") {
-      const auto routeInfo = manager.routeInfo(
+      const auto busInfo = manager.busInfo(
           reqMap.at(JSON_KEYS.at(JsonKeys::NAME)).AsString(), id);
-      outStream << printReq(routeInfo);
-    } else {
+      outStream << printReq(busInfo);
+    } else if (type == "Stop") {
       const auto stopInfo = manager.stopInfo(
           reqMap.at(JSON_KEYS.at(JsonKeys::NAME)).AsString(), id);
       outStream << printReq(stopInfo);
+    } else if (type == "Route") {
+      //      const auto& from = reqMap.at("from").AsString();
+      //      const auto& to = reqMap.at("to").AsString();
+      //      const auto p = make_pair(from, to);
+      // TODO: print route info
     }
   }
   outStream << "]";
-}
-
-void testJson() {
-  string str = R"({
-               "type": "Bus",
-               "is_roundtrip": true,
-               "name": "256",
-               "stops": [
-                 "Biryulyovo Zapadnoye",
-                 "Biryusinka",
-                 "Universam",
-                 "Biryulyovo Tovarnaya",
-                 "Biryulyovo Passazhirskaya",
-                 "Biryulyovo Zapadnoye"
-               ]
-             })";
-
-  istringstream in_stream(str);
-  auto doc = Json::Load(in_stream);
-  const auto& root = doc.GetRoot();
-  auto parsedResult = root.AsMap();
-
-  cout << "name: " << parsedResult["name"].AsString() << endl;
-
-  auto route = buildRoute(parsedResult);
-  cout << "route num: " << route.routeNumber() << endl;
-}
-
-void testLoadVal() {
-  string str =
-      "{"
-      "\"value\": -15.42,"
-      "\"id\": 21,"
-      "\"latitude\": 55.61"
-      "}";
-  istringstream in_stream(str);
-  auto doc = Json::Load(in_stream);
-  const auto& root = doc.GetRoot();
-  auto parsedRes = root.AsMap();
-  cout << "value:_" << parsedRes["value"].AsDouble() << endl;
-  cout << "id:_" << parsedRes["id"].AsInt() << endl;
-  cout << "latitude:_" << parsedRes["latitude"].AsDouble() << endl;
 }
 
 void testTransportDPart() {
@@ -578,10 +652,11 @@ void testTransportDPart() {
   istringstream in_stream(str);
   auto doc = Json::Load(in_stream);
   const auto& rootMap = doc.GetRoot().AsMap();
-  auto manager =
-      buildDataBase(rootMap.at(JSON_KEYS.at(JsonKeys::BASE_REQS)).AsArray());
+  auto catalog =
+      buildCatalog(rootMap.at(JSON_KEYS.at(JsonKeys::BASE_REQS)).AsArray(),
+                   rootMap.at(JSON_KEYS.at(JsonKeys::ROUTE_SETTS)).AsMap());
   ostringstream out_stream;
-  processRequests(manager,
+  processRequests(catalog,
                   rootMap.at(JSON_KEYS.at(JsonKeys::STAT_REQS)).AsArray(),
                   out_stream);
 
@@ -591,13 +666,15 @@ void testTransportDPart() {
 int main() {
   auto doc = Json::Load(cin);
   const auto& rootMap = doc.GetRoot().AsMap();
-  auto manager = buildDataBase(rootMap.at("base_requests").AsArray());
-  processRequests(manager, rootMap.at("stat_requests").AsArray());
+  auto catalog =
+      buildCatalog(rootMap.at(JSON_KEYS.at(JsonKeys::BASE_REQS)).AsArray(),
+                   rootMap.at(JSON_KEYS.at(JsonKeys::ROUTE_SETTS)).AsMap());
+
+  processRequests(catalog,
+                  rootMap.at(JSON_KEYS.at(JsonKeys::STAT_REQS)).AsArray());
 
   //  TestRunner tr;
   //  RUN_TEST(tr, testTransportDPart);
-  //  RUN_TEST(tr, testJson);
-  //  RUN_TEST(tr, testLoadVal);
 
   return 0;
 }
